7373ee6278d44a9f75be4bd72a6a577bd8fc3dc8
CarlosAndres <solrak41@gmail.com>
2022-08-19 16:13:24 -0500

    Add getComputerChoice function

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 const rockPaperOrScissors = ["Rock", "Paper", "Scissors"]
-console.log(rockPaperOrScissors);
+
+function getComputerChoice(array) {
+    // Generate a random number beteen 0 and 2
+    let randomNumber = Math.floor(Math.random() * array.length)
+    let computerChoice = array[randomNumber]
+    console.log(computerChoice);
+}
+
+getComputerChoice(rockPaperOrScissors)
