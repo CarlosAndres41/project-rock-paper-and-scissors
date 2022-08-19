70a811cfa210025bc85f4a40b072c6f636c02167
CarlosAndres <solrak41@gmail.com>
2022-08-19 16:57:24 -0500

    Create a function that plays a single round. Check if there's a tie

--- index.js
+++ index.js
@@ -6,10 +6,26 @@ function getComputerChoice() {
     return rockPaperOrScissors[randomNumber]
 }
 
+// Assign getComputerChoice to a variable 
+let computerSelection = getComputerChoice()
+
 // Add the option for a player to pick rock, paper or scissors
 let playerSelection = prompt("Write your selection. Rock, Paper or Scissors:")
 
 // Format player selection
 let lowerCase = playerSelection.toLowerCase()
 let playerSelectionFormatted = `${lowerCase[0].toUpperCase()}${lowerCase.slice(1)}`
-console.log(playerSelectionFormatted);
+
+// Create a function that plays a single round
+function playGame(playerSelection, computerSelection) {
+    let player = playerSelection;
+    let pc = computerSelection;
+
+    if (player === pc) {
+        return `It's a tie, both players picked: ${player}.`
+    } else {
+        return "There was a winner."
+    }
+}
+
+console.log(playGame(playerSelectionFormatted, computerSelection));
